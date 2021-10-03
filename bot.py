72d2b099b1a5fb42b04806bb91457a05735fbc1e
wrjones104 <wrjones@gmail.com>
2021-10-03 14:22:05 -0400

    Added custom support

--- bot.py
+++ bot.py
@@ -5,6 +5,9 @@ import random
 from create import rollseed
 from create import get_chaos
 from create import get_truechaos
+from dotenv import load_dotenv
+
+load_dotenv()
 
 client = discord.Client()
 
@@ -22,19 +25,19 @@ async def on_message(message):
 
     if message.content.startswith('!rando'):
         r=rollseed()
-        await message.channel.send("Here's your seed! Have fun!")
+        await message.channel.send("[BETA Here's your seed! Have fun!")
         await message.channel.send(r['share_url'])
         #await message.channel.send("And the flags, too:")
         #await message.channel.send(r['flags'])
 
     if message.content.startswith('!chaos'):
         r = get_chaos()
-        await message.channel.send("Oh, you're a feisty one, eh?")
+        await message.channel.send("[BETA] Oh, you're a feisty one, eh?")
         await message.channel.send(r['share_url'])
 
     if message.content.startswith('!truechaos'):
         r=get_truechaos()
-        await message.channel.send("So you have chosen death...")
+        await message.channel.send("[BETA] So you have chosen death...")
         await message.channel.send(r['share_url'])
 
-client.run(os.getenv('TOKEN'))
+client.run(os.getenv('DISCORD_TOKEN'))
