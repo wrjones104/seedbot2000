9b36d362d830f3cdc72fe884541c3330f036f2f3
Will Jones <wrjones@gmail.com>
2022-01-17 22:45:26 -0500

    More cleanup for v.1.0

--- bot.py
+++ bot.py
@@ -1,9 +1,6 @@
-import sys
 import random
 import discord
-
 import create
-import flags
 import os
 import datetime
 import json
@@ -14,8 +11,7 @@ from bingo.randomize_drops import run_item_rando
 from discord.ext import tasks
 from maths import get_cr
 from dotenv import load_dotenv
-from create import generate_random_seed, cr_search, generate_hard_chaos_seed, generate_easy_chaos_seed, getlink, generate_v1_seed
-from custom_sprites_portraits import spraypaint
+from create import generate_v1_seed
 
 stream_msg = {}
 load_dotenv()
@@ -27,114 +23,114 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     # When SeedBot logs in, it's going to prepare all "live stream" channels for the getstreams function by clearing
     # all previous messages from itself and posting an initial message which will act as the "edit" anchor
-#     def is_me(m):
-#         return m.author == client.user
-#     with open('db/suppressed_servers.txt') as f:
-#         suppressed_servers = f.readlines()
-#     with open('db/streambot_channels.json') as f:
-#         streambot_channels = json.load(f)
-#     for a in streambot_channels:
-#         if str(streambot_channels[a]['server_id']) in suppressed_servers:
-#             continue
-#         else:
-#             clean_channel = client.get_channel(streambot_channels[a]['channel_id'])
-#         await clean_channel.purge(check=is_me)
-#         await clean_channel.send("Initializing...")
-#     getstreams.start()
-#
-#
-# @tasks.loop(minutes=1)
-# async def getstreams():
-#     # We're just going to load a bunch of files into variables. We're doing this here so that it reads the files on
-#     # every loop, which allows us to edit the files while the bot is running. This is helpful for if we want to add
-#     # channels, categories or keywords without having to restart the bot.
-#     with open('db/game_cats.json') as f:
-#         game_cats = json.load(f)
-#     with open('db/streambot_channels.json') as f:
-#         streambot_channels = json.load(f)
-#     with open('db/suppressed_servers.txt') as f:
-#         suppressed_servers = f.readlines()
-#     global stream_msg
-#     streamlist = ''
-#     # This next part searches the Twitch API for all categories and keywords that are specified in the
-#     # "game_cats.json" file
-#     for gc in game_cats:
-#         conn = http.client.HTTPSConnection("api.twitch.tv")
-#         payload = ''
-#         headers = {
-#             'Client-ID': os.getenv('client_id'),
-#             'Authorization': os.getenv('twitch_token')
-#         }
-#         conn.request("GET", "/helix/streams?game_id=" + str(gc) + "&first=100", payload, headers)
-#         res = conn.getresponse()
-#         data = res.read()
-#         x = data.decode("utf-8")
-#         # Twitch's API requires a refreshed token every 90 days. Chances are, I'm going to forget about this so this
-#         # message is a reminder if that happens! :)
-#         if "Invalid OAuth token" in x:
-#             streamlist = "Twitch OAuth token expired. Tell Jones!"
-#             break
-#         j = json.loads(x)
-#         xx = j['data']
-#         if not j['pagination']:
-#             empty_page = True
-#             pag = ""
-#         else:
-#             pag = j['pagination']['cursor']
-#             empty_page = False
-#         # Twitch's API will only return 100 streams max per call along with a "cursor" which you can use in a
-#         # follow-up call to get the next 100 streams. This part just loops through all "pages" until it reaches an
-#         # empty one (which means it's at the end)
-#         while not empty_page:
-#             conn.request("GET", "/helix/streams?game_id=" + str(gc) + "&first=100&after="+str(pag), payload, headers)
-#             res = conn.getresponse()
-#             data = res.read()
-#             x = data.decode("utf-8")
-#             j = json.loads(x)
-#             try:
-#                 if not j['pagination']:
-#                     empty_page = True
-#                     pass
-#                 else:
-#                     pag = j['pagination']['cursor']
-#                     xx += j['data']
-#             except KeyError:
-#                 print(j)
-#         k = len(xx)
-#         # This part takes k (the amount of streams returned total) and uses it to iterate through all the returned
-#         # streams to find any with keywords from the "game_cats.json" file in the title of the stream
-#         while k != 0:
-#             if any(ac in xx[k - 1]['title'].lower() for ac in game_cats[gc]['keywords']):
-#                 aa = xx[k - 1]
-#                 streamlist += f'**{aa["user_name"]}** is streaming! --- <https://twitch.tv/{aa["user_name"]}>\n' \
-#                               f'```Title:       {aa["title"].rstrip()}\nCategory:    {aa["game_name"]}\n' \
-#                               f'Stream Time: ' \
-#                               f'{str(datetime.datetime.utcnow() - datetime.datetime.strptime(aa["started_at"], "%Y-%m-%dT%H:%M:%SZ")).split(".")[0]}```\n'
-#             k -= 1
-#     streamlistmsg = f'I found some active streams! Show some love by joining in and following FF6WC' \
-#                  f' streamers!\n\n' + streamlist
-#     # Next, we're going to send the stream list to all the channels in the "streambot_channels.json" file. If there
-#     # are no streams, we're going to send a specific message. If the stream list hasn't changed since the last time
-#     # we checked, we're not going to do anything
-#     for sc in streambot_channels:
-#         if str(streambot_channels[sc]['server_id']) in suppressed_servers:
-#             continue
-#         else:
-#             channel = client.get_channel(streambot_channels[sc]['channel_id'])
-#         if channel.id not in stream_msg.keys():
-#             stream_msg[channel.id] = await channel.fetch_message(channel.last_message_id)
-#         if streamlist == '':
-#             await stream_msg[channel.id].edit(content=functions.sad_day())
-#             f = open('db/gs_msg.txt', 'w')
-#             f.write(functions.sad_day())
-#             f.close()
-#         elif streamlist == stream_msg[channel.id]:
-#             pass
-#         else:
-#             await stream_msg[channel.id].edit(content=streamlistmsg)
-#             f = open('db/gs_msg.txt', 'w')
-#             f.write(streamlistmsg)
-#             f.close()
+    def is_me(m):
+        return m.author == client.user
+    with open('db/suppressed_servers.txt') as f:
+        suppressed_servers = f.readlines()
+    with open('db/streambot_channels.json') as f:
+        streambot_channels = json.load(f)
+    for a in streambot_channels:
+        if str(streambot_channels[a]['server_id']) in suppressed_servers:
+            continue
+        else:
+            clean_channel = client.get_channel(streambot_channels[a]['channel_id'])
+        await clean_channel.purge(check=is_me)
+        await clean_channel.send("Initializing...")
+    getstreams.start()
+
+
+@tasks.loop(minutes=1)
+async def getstreams():
+    # We're just going to load a bunch of files into variables. We're doing this here so that it reads the files on
+    # every loop, which allows us to edit the files while the bot is running. This is helpful for if we want to add
+    # channels, categories or keywords without having to restart the bot.
+    with open('db/game_cats.json') as f:
+        game_cats = json.load(f)
+    with open('db/streambot_channels.json') as f:
+        streambot_channels = json.load(f)
+    with open('db/suppressed_servers.txt') as f:
+        suppressed_servers = f.readlines()
+    global stream_msg
+    streamlist = ''
+    # This next part searches the Twitch API for all categories and keywords that are specified in the
+    # "game_cats.json" file
+    for gc in game_cats:
+        conn = http.client.HTTPSConnection("api.twitch.tv")
+        payload = ''
+        headers = {
+            'Client-ID': os.getenv('client_id'),
+            'Authorization': os.getenv('twitch_token')
+        }
+        conn.request("GET", "/helix/streams?game_id=" + str(gc) + "&first=100", payload, headers)
+        res = conn.getresponse()
+        data = res.read()
+        x = data.decode("utf-8")
+        # Twitch's API requires a refreshed token every 90 days. Chances are, I'm going to forget about this so this
+        # message is a reminder if that happens! :)
+        if "Invalid OAuth token" in x:
+            streamlist = "Twitch OAuth token expired. Tell Jones!"
+            break
+        j = json.loads(x)
+        xx = j['data']
+        if not j['pagination']:
+            empty_page = True
+            pag = ""
+        else:
+            pag = j['pagination']['cursor']
+            empty_page = False
+        # Twitch's API will only return 100 streams max per call along with a "cursor" which you can use in a
+        # follow-up call to get the next 100 streams. This part just loops through all "pages" until it reaches an
+        # empty one (which means it's at the end)
+        while not empty_page:
+            conn.request("GET", "/helix/streams?game_id=" + str(gc) + "&first=100&after="+str(pag), payload, headers)
+            res = conn.getresponse()
+            data = res.read()
+            x = data.decode("utf-8")
+            j = json.loads(x)
+            try:
+                if not j['pagination']:
+                    empty_page = True
+                    pass
+                else:
+                    pag = j['pagination']['cursor']
+                    xx += j['data']
+            except KeyError:
+                print(j)
+        k = len(xx)
+        # This part takes k (the amount of streams returned total) and uses it to iterate through all the returned
+        # streams to find any with keywords from the "game_cats.json" file in the title of the stream
+        while k != 0:
+            if any(ac in xx[k - 1]['title'].lower() for ac in game_cats[gc]['keywords']):
+                aa = xx[k - 1]
+                streamlist += f'**{aa["user_name"]}** is streaming! --- <https://twitch.tv/{aa["user_name"]}>\n' \
+                              f'```Title:       {aa["title"].rstrip()}\nCategory:    {aa["game_name"]}\n' \
+                              f'Stream Time: ' \
+                              f'{str(datetime.datetime.utcnow() - datetime.datetime.strptime(aa["started_at"], "%Y-%m-%dT%H:%M:%SZ")).split(".")[0]}```\n'
+            k -= 1
+    streamlistmsg = f'I found some active streams! Show some love by joining in and following FF6WC' \
+                 f' streamers!\n\n' + streamlist
+    # Next, we're going to send the stream list to all the channels in the "streambot_channels.json" file. If there
+    # are no streams, we're going to send a specific message. If the stream list hasn't changed since the last time
+    # we checked, we're not going to do anything
+    for sc in streambot_channels:
+        if str(streambot_channels[sc]['server_id']) in suppressed_servers:
+            continue
+        else:
+            channel = client.get_channel(streambot_channels[sc]['channel_id'])
+        if channel.id not in stream_msg.keys():
+            stream_msg[channel.id] = await channel.fetch_message(channel.last_message_id)
+        if streamlist == '':
+            await stream_msg[channel.id].edit(content=functions.sad_day())
+            f = open('db/gs_msg.txt', 'w')
+            f.write(functions.sad_day())
+            f.close()
+        elif streamlist == stream_msg[channel.id]:
+            pass
+        else:
+            await stream_msg[channel.id].edit(content=streamlistmsg)
+            f = open('db/gs_msg.txt', 'w')
+            f.write(streamlistmsg)
+            f.close()
 
 
 @client.event
