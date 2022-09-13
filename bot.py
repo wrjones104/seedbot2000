ddfc923358f85a3d64248bf943a16fe41d65cc4a
Will <wrjones104>
2022-09-13 12:28:48 -0400

    small tweak to preset commands to prevent interference with dr-race-bot

--- bot.py
+++ bot.py
@@ -1,25 +1,42 @@
 import os
 
 import discord
+from typing import Literal
+from discord import app_commands, Interaction, TextStyle
+from discord.ui import Modal, TextInput
 from dotenv import load_dotenv
-
+from func import command_functions as cf
 import parse_commands
 
 load_dotenv()
-client = discord.Client()
 
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+class aclient(discord.Client):
+    def __init__(self):
+        super().__init__(intents=discord.Intents.all())
+        self.synced = False
+
+    async def on_ready(self):
+        await self.wait_until_ready()
+        if not self.synced:
+            await tree.sync()
+            self.synced = True
+        print(f"We have logged in as {self.user}.")
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith("!"):
-        await parse_commands.parse_bot_command(message)
+client = aclient()
+tree = app_commands.CommandTree(client)
+
+
+@tree.command(name="help", description="SeedBot help")
+@app_commands.describe(type='What do you want help with?')
+async def help(interaction: Interaction, type: Literal['general', 'presets', 'dev']):
+    if type == 'presets':
+        await cf.preset_help(interaction)
+    elif type == 'dev':
+        await cf.dev_help(interaction)
+    else:
+        await cf.gen_help(interaction)
 
 
 client.run(os.getenv('DISCORD_TOKEN'))
