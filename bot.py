089a18f983a18bad68b06e6ec7f07126072fea19
Will <wrjones104>
2022-09-13 13:22:59 -0400

    Adding first slash command - /help

--- bot.py
+++ bot.py
@@ -1,25 +1,41 @@
 import os
+from typing import Literal
 
 import discord
+from discord import app_commands, Interaction
 from dotenv import load_dotenv
 
-import parse_commands
+from func import command_functions as cf
 
 load_dotenv()
-client = discord.Client()
 
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+class aclient(discord.Client):
+    def __init__(self):
+        super().__init__(intents=discord.Intents.all())
+        self.synced = False
+
+    async def on_ready(self):
+        await self.wait_until_ready()
+        if not self.synced:
+            await tree.sync()
+            self.synced = True
+        print(f"We have logged in as {self.user}.")
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith("!"):
-        await parse_commands.parse_bot_command(message)
+client = aclient()
+tree = app_commands.CommandTree(client)
+
+
+@tree.command(name="help", description="SeedBot help")
+@app_commands.describe(type='What do you want help with?')
+async def help(interaction: Interaction, type: Literal['general', 'presets', 'dev']):
+    if type == 'presets':
+        await cf.preset_help(interaction)
+    elif type == 'dev':
+        await cf.dev_help(interaction)
+    else:
+        await cf.gen_help(interaction)
 
 
 client.run(os.getenv('DISCORD_TOKEN'))
