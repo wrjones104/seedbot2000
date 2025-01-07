5a802b39c089b2ae036bbf1d1b33438751ac7a8c
wrjones104 <wrjones@gmail.com>
2025-01-07 11:37:55 -0500

    fixed timeout issue for linux systems

--- run_local.py
+++ run_local.py
@@ -1,31 +1,28 @@
 import subprocess
-
+import os
+import signal
 
 async def local_wc(flags, beta, filename):
+    args = ("python3 wc.py -i ../WorldsCollide/ff3.smc -o ../WorldsCollide/seeds/" + filename + ".smc " + flags)
+
     if beta in ("dev", "new"):
         rolldir = "WorldsCollide_dev/"
-        args = (
-            "python3 wc.py -i ../WorldsCollide/ff3.smc -o ../WorldsCollide/seeds/"
-            + filename
-            + ".smc "
-            + flags
-        )
     elif beta == "practice":
         rolldir = 'WorldsCollide_practice/'
-        args = ("python3 wc.py -i ../WorldsCollide/ff3.smc -o ../WorldsCollide/seeds/" + filename + ".smc " + flags)
     elif beta == "doors":
         rolldir = 'WorldsCollide_Door_Rando/'
-        args = ("python3 wc.py -i ../WorldsCollide/ff3.smc -o ../WorldsCollide/seeds/" + filename + ".smc " + flags)
     elif beta == "lg1" or beta == "lg2":
         rolldir = 'WorldsCollide_location_gating1/'
-        args = ("python3 wc.py -i ../WorldsCollide/ff3.smc -o ../WorldsCollide/seeds/" + filename + ".smc " + flags)
     else:
         rolldir = "WorldsCollide/"
-        args = "python3 wc.py -i ff3.smc -o seeds/" + filename + ".smc " + flags
     try:
-        localdata = subprocess.check_output(args, cwd=rolldir, shell=True, timeout=30)
-        return localdata
+        localdata = subprocess.Popen(args, cwd=rolldir, shell=True, start_new_session=True, stdout=subprocess.PIPE)
+        localdata.wait(timeout=10)
+        out = localdata.communicate()
+        print(f'out={out[0]}')
+        return out[0]
     except subprocess.TimeoutExpired as e:
+        os.killpg(os.getpgid(localdata.pid), signal.SIGTERM)
         raise e
     except (subprocess.CalledProcessError, FileNotFoundError) as e:
         raise e
